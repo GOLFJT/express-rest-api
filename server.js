37d14e23d192ef9c7b7ce21d61111522e8a04bce
jirawat <jirawat@thinknet.co.th>
2017-09-14 17:52:42 +0700

    server.js

--- server.js
+++ server.js
@@ -87,4 +87,36 @@ app.delete('/product/:id', function (req, res) {
     
 })
 
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.get('/books', (req, res) => {
+//   res.json(books)
+// })
+
+// app.get('/books/:id', (req, res) => {
+//   res.json(books.find(book => book.id === req.params.id))
+// })
+
+// app.post('/books', (req, res) => {
+//   books.push(req.body)
+//   res.status(201).json(req.body)
+// })
+
+// app.put('/books/:id', (req, res) => {
+//   const updateIndex = books.findIndex(book => book.id === req.params.id)
+//   res.json(Object.assign(books[updateIndex], req.body))
+// })
+
+// app.delete('/books/:id', (req, res) => {
+//   const deleteIndex = books.findIndex(book => book.id === req.params.id)
+//   books.splice(deleteIndex, 1)
+//   res.status(204).send()
+// })
+
+// app.listen(3000, () => {
+//   console.log('Start server at port 3000.')
+// })
+
 app.listen(3001)
